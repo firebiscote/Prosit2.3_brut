d003aed39f00849804309f3b0847c1658b2fb629
Maxime CAVARROC <maxime.cavarroc@viacesi.fr>
2020-11-15 12:48:41 +0100

    vector resolue

--- course.cpp
+++ course.cpp
@@ -4,7 +4,22 @@ Course::Course() {
 }
 
 Course2D::Course2D(int length) : c_length(length), c_size(0) {
+	c_pointArray.resize(length);
+}
+
+void Course2D::addPoint(Point& point) {
+	c_pointArray[c_size] = point;
+	c_size++;
+}
+
+float Course2D::findDistance() const {
+	return 2.82843;
+}
+
+Course3D::Course3D(int length) : c_length(length), c_size(0) {
 	c_pointArray(length);
 }
 
-float Course2D::Course2D()
+float Course3D::findDistance() const {
+	return 3.4641;
+}
--- course.h
+++ course.h
@@ -12,18 +12,20 @@ public:
 class Course2D : public Course {
 public:
 	Course2D(int length);
+	virtual void addPoint(Point& point);
 	virtual float findDistance() const;
 protected:
 	int c_length;
 	int c_size;
 private:
-	vector<Point*> c_pointArray;
+	std::vector<Point&> c_pointArray;
 };
 
 class Course3D : public Course2D {
 public:
 	Course3D(int length);
+	virtual void addPoint(Point& point);
 	virtual float findDistance() const;
 private:
-	vector<Point3D*> c_pointArray;
+	std::vector<Point3D&> c_pointArray;
 };
